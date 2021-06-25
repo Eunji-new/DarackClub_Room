7ea453e04a6028b438d8642e63e1a4b8e8db813c
dmswl9898 <sejj0126@naver.com>
2021-06-25 23:38:54 +0900

    add move in window

---
+++ Assets/cameraRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotate : MonoBehaviour
+{
+    private float speed_rota = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.Rotate(Vector3.up * speed_rota * mouseX);
+        transform.Rotate(Vector3.left * speed_rota * mouseY);
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveObjectFunc();
+    }
+
+
+    private float speed_move = 3.0f;
+
+    void moveObjectFunc()
+    {
+        float keyH = Input.GetAxis("Horizontal");
+        float keyV = Input.GetAxis("Vertical");
+        keyH = keyH * speed_move * Time.deltaTime;
+        keyV = keyV * speed_move * Time.deltaTime;
+        transform.Translate(Vector3.right * keyH);
+        transform.Translate(Vector3.forward * keyV);
+
+    }
+
+
+}
